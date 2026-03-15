f5798326716850ef4c9049099370e3c7744e4182
mudoleto <cameronsandoval83@gmail.com>
2026-03-15 01:13:55 -0600

    Erease views.py from core

--- core/urls.py
+++ core/urls.py
@@ -19,9 +19,8 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from .view import TestView
+
 
 urlpatterns = [
-    path('test/', TestView.as_view()),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
